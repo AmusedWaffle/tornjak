410f27072cea7b8d1031ad0f203ae60747f05b44
Mason Muniz <darkevix101@gmail.com>
2025-04-13 16:06:30 -0400

    -Separating functions from handlers.go into other files 	-cluster_handlers.go 	-federation_handlers.go -Should now be ready to start changing individual functions to use refactored code
    Signed-off-by: Mason Muniz <darkevix101@gmail.com>

--- api/agent/handlers.go
+++ api/agent/handlers.go
@@ -6,9 +6,6 @@ import (
 	"io"
 	"net/http"
 	"strings"
-
-	trustdomain "github.com/spiffe/spire-api-sdk/proto/spire/api/server/trustdomain/v1"
-	"google.golang.org/protobuf/encoding/protojson"
 )
 
 // Checks for httpError; meant to replace the (about) 4 chunks of error checking code run per function
@@ -50,16 +47,17 @@ func copyIntoBuff(buf *strings.Builder, w http.ResponseWriter, r *http.Request, 
 // Gets called from GetRouter func in server.go
 // http is a type of command that the server can take that starts the server
 // Gives an explicit return only if an error is countered
-// otherwise, since no return is outlined, no return is necessary
+	// otherwise, since no return is outlined, no return is necessary
 func (s *Server) healthcheck(w http.ResponseWriter, r *http.Request) {
-
-	//To store the HealthcheckRequest we get from grpc
+	
+	// To store the HealthcheckRequest we get from grpc
 	var input HealthcheckRequest
 	buf := new(strings.Builder)
 
-	//copies body of http.Request into buf
-	//and then puts the # of bytes of type int64 into n and an error into err
-	//err is nil if there was no error
+	// copies body of http.Request into buf
+	// and then puts the # of bytes of type int64 into n and an error into err
+	// err is nil if there was no error
+	// n is 0 and data is "err" if err was not nil inside of copyIntoBuff
 	n, err := io.Copy(buf, r.Body)
 	if err != nil {
 		emsg := fmt.Sprintf("Error parsing data: %v", err.Error())
@@ -68,12 +66,12 @@ func (s *Server) healthcheck(w http.ResponseWriter, r *http.Request) {
 	}
 	data := buf.String()
 
-	//if the body of the request was empty
+	// if the body of the request was empty
 	if n == 0 {
 		input = HealthcheckRequest{}
 
-	//else if the body was NOT empty
-	//unmarshal the JSON and check for errors
+	// else if the body was NOT empty
+	// unmarshal the JSON and check for errors
 	} else {
 		err := json.Unmarshal([]byte(data), &input)
 		if err != nil {
@@ -83,8 +81,8 @@ func (s *Server) healthcheck(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	//ret gets the address to the HealthcheckReponse response
-		//the HealthcheckResponse is actually the response of the server "input" as returned by grpc's HealthClient.Check()
+	// ret gets the address to the HealthcheckReponse response
+		// the HealthcheckResponse is actually the response of the server "input" as returned by grpc's HealthClient.Check()
 	ret, err := s.SPIREHealthcheck(input) //nolint:govet //Ignoring mutex (not being used) - sync.Mutex by value is unused for linter govet
 	if err != nil {
 		emsg := fmt.Sprintf("Error: %v", err.Error())
@@ -92,7 +90,7 @@ func (s *Server) healthcheck(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//Sets the headers associated with the request to specific values
+	// Sets the headers associated with the request to specific values
 	/*
 		"Content-Type" --> "application/json;charset=UTF-8"
 		"Access-Control-Allow-Origin" --> "*"
@@ -102,22 +100,22 @@ func (s *Server) healthcheck(w http.ResponseWriter, r *http.Request) {
 	*/
 	cors(w, r)
 
-	//Creates an Encoder object pointer that will be writing to http.ResponseWriter w
+	// Creates an Encoder object pointer that will be writing to http.ResponseWriter w
 	je := json.NewEncoder(w)
 
-	//writing the JSON encoing of ret 
-		//(address to the HealtheckResponse of whatever server we're checking) 
-		//to http.ResponseWriter w
+	// writing the JSON encoing of ret 
+		// (address to the HealtheckResponse of whatever server we're checking) 
+		// to http.ResponseWriter w
 	err = je.Encode(ret)
-	
-	//if the Encode failed
+
+	// if the Encode failed
 	if err != nil {
 		emsg := fmt.Sprintf("Error: %v", err.Error())
 		retError(w, emsg, http.StatusBadRequest)
 		return
 	}
 
-	//if we never return an error, then all is well!
+	// if we never return an error, then all is well!
 }
 
 func (s *Server) debugServer(w http.ResponseWriter, r *http.Request) {
@@ -635,173 +633,6 @@ func (s *Server) federatedBundleDelete(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// Federation APIs
-func (s *Server) federationList(w http.ResponseWriter, r *http.Request) {
-	var input ListFederationRelationshipsRequest
-	buf := new(strings.Builder)
-
-	n, err := io.Copy(buf, r.Body)
-	if err != nil {
-		emsg := fmt.Sprintf("Error parsing data: %v", err.Error())
-		retError(w, emsg, http.StatusBadRequest)
-		return
-	}
-	data := buf.String()
-
-	if n == 0 {
-		input = ListFederationRelationshipsRequest{}
-	} else {
-		err := json.Unmarshal([]byte(data), &input)
-		if err != nil {
-			emsg := fmt.Sprintf("Error parsing data: %v", err.Error())
-			retError(w, emsg, http.StatusBadRequest)
-			return
-		}
-	}
-
-	ret, err := s.ListFederationRelationships(input) //nolint:govet //Ignoring mutex (not being used) - sync.Mutex by value is unused for linter govet
-	if err != nil {
-		emsg := fmt.Sprintf("Error: %v", err.Error())
-		retError(w, emsg, http.StatusInternalServerError)
-		return
-	}
-
-	cors(w, r)
-	je := json.NewEncoder(w)
-	err = je.Encode(ret)
-	if err != nil {
-		emsg := fmt.Sprintf("Error: %v", err.Error())
-		retError(w, emsg, http.StatusBadRequest)
-		return
-	}
-}
-
-func (s *Server) federationCreate(w http.ResponseWriter, r *http.Request) {
-	var input CreateFederationRelationshipRequest
-	var rawInput trustdomain.BatchCreateFederationRelationshipRequest
-	buf := new(strings.Builder)
-
-	n, err := io.Copy(buf, r.Body)
-	if err != nil {
-		emsg := fmt.Sprintf("Error parsing data: %v", err.Error())
-		retError(w, emsg, http.StatusBadRequest)
-		return
-	}
-	data := buf.String()
-
-	if n == 0 {
-		input = CreateFederationRelationshipRequest{}
-	} else {
-		// required to use protojson because of oneof field
-		err := protojson.Unmarshal([]byte(data), &rawInput)
-		if err != nil {
-			emsg := fmt.Sprintf("Error parsing data: %v", err.Error())
-			retError(w, emsg, http.StatusBadRequest)
-			return
-		}
-		input = CreateFederationRelationshipRequest(rawInput) //nolint:govet //Ignoring mutex (not being used) - sync.Mutex by value is unused for linter govet
-	}
-
-	ret, err := s.CreateFederationRelationship(input) //nolint:govet //Ignoring mutex (not being used) - sync.Mutex by value is unused for linter govet
-	if err != nil {
-		emsg := fmt.Sprintf("Error: %v", err.Error())
-		retError(w, emsg, http.StatusInternalServerError)
-		return
-	}
-
-	cors(w, r)
-	je := json.NewEncoder(w)
-	err = je.Encode(ret)
-	if err != nil {
-		emsg := fmt.Sprintf("Error: %v", err.Error())
-		retError(w, emsg, http.StatusBadRequest)
-		return
-	}
-}
-
-func (s *Server) federationUpdate(w http.ResponseWriter, r *http.Request) {
-	var input UpdateFederationRelationshipRequest
-	var rawInput trustdomain.BatchUpdateFederationRelationshipRequest
-	buf := new(strings.Builder)
-
-	n, err := io.Copy(buf, r.Body)
-	if err != nil {
-		emsg := fmt.Sprintf("Error parsing data: %v", err.Error())
-		retError(w, emsg, http.StatusBadRequest)
-		return
-	}
-	data := buf.String()
-
-	if n == 0 {
-		input = UpdateFederationRelationshipRequest{}
-	} else {
-		err := protojson.Unmarshal([]byte(data), &rawInput)
-		if err != nil {
-			emsg := fmt.Sprintf("Error parsing data: %v", err.Error())
-			retError(w, emsg, http.StatusBadRequest)
-			return
-		}
-		input = UpdateFederationRelationshipRequest(rawInput) //nolint:govet //Ignoring mutex (not being used) - sync.Mutex by value is unused for linter govet
-
-	}
-
-	ret, err := s.UpdateFederationRelationship(input) //nolint:govet //Ignoring mutex (not being used) - sync.Mutex by value is unused for linter govet
-	if err != nil {
-		emsg := fmt.Sprintf("Error: %v", err.Error())
-		retError(w, emsg, http.StatusInternalServerError)
-		return
-	}
-
-	cors(w, r)
-	je := json.NewEncoder(w)
-	err = je.Encode(ret)
-	if err != nil {
-		emsg := fmt.Sprintf("Error: %v", err.Error())
-		retError(w, emsg, http.StatusBadRequest)
-		return
-	}
-}
-
-func (s *Server) federationDelete(w http.ResponseWriter, r *http.Request) {
-	var input DeleteFederationRelationshipRequest
-	buf := new(strings.Builder)
-
-	n, err := io.Copy(buf, r.Body)
-	if err != nil {
-		emsg := fmt.Sprintf("Error parsing data: %v", err.Error())
-		retError(w, emsg, http.StatusBadRequest)
-		return
-	}
-	data := buf.String()
-
-	if n == 0 {
-		input = DeleteFederationRelationshipRequest{}
-	} else {
-		err := json.Unmarshal([]byte(data), &input)
-		if err != nil {
-			emsg := fmt.Sprintf("Error parsing data: %v", err.Error())
-			retError(w, emsg, http.StatusBadRequest)
-			return
-		}
-	}
-
-	ret, err := s.DeleteFederationRelationship(input) //nolint:govet //Ignoring mutex (not being used) - sync.Mutex by value is unused for linter govet
-	if err != nil {
-		emsg := fmt.Sprintf("Error: %v", err.Error())
-		retError(w, emsg, http.StatusInternalServerError)
-		return
-	}
-
-	cors(w, r)
-	je := json.NewEncoder(w)
-	err = je.Encode(ret)
-	if err != nil {
-		emsg := fmt.Sprintf("Error: %v", err.Error())
-		retError(w, emsg, http.StatusBadRequest)
-		return
-	}
-}
-
 // Tornjak Handlers
 func (s *Server) home(w http.ResponseWriter, r *http.Request) {
 	var ret = "Welcome to the Tornjak Backend!"
@@ -935,152 +766,3 @@ func (s *Server) tornjakAgentsList(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
-
-/********* CLUSTER *********/
-
-func (s *Server) clusterList(w http.ResponseWriter, r *http.Request) {
-	var input ListClustersRequest
-	buf := new(strings.Builder)
-
-	n, err := io.Copy(buf, r.Body)
-	if err != nil {
-		emsg := fmt.Sprintf("Error parsing data: %v", err.Error())
-		retError(w, emsg, http.StatusBadRequest)
-		return
-	}
-	data := buf.String()
-
-	if n == 0 {
-		input = ListClustersRequest{}
-	} else {
-		err := json.Unmarshal([]byte(data), &input)
-		if err != nil {
-			emsg := fmt.Sprintf("Error parsing data: %v", err.Error())
-			retError(w, emsg, http.StatusBadRequest)
-			return
-		}
-	}
-
-	ret, err := s.ListClusters(input)
-	if err != nil {
-		emsg := fmt.Sprintf("Error: %v", err.Error())
-		retError(w, emsg, http.StatusBadRequest)
-		return
-	}
-	cors(w, r)
-	je := json.NewEncoder(w)
-	err = je.Encode(ret)
-	if err != nil {
-		emsg := fmt.Sprintf("Error: %v", err.Error())
-		retError(w, emsg, http.StatusBadRequest)
-		return
-	}
-}
-
-func (s *Server) clusterCreate(w http.ResponseWriter, r *http.Request) {
-	buf := new(strings.Builder)
-	n, err := io.Copy(buf, r.Body)
-	if err != nil {
-		emsg := fmt.Sprintf("Error parsing data: %v", err.Error())
-		retError(w, emsg, http.StatusBadRequest)
-		return
-	}
-	data := buf.String()
-	var input RegisterClusterRequest
-	if n == 0 {
-		input = RegisterClusterRequest{}
-	} else {
-		err := json.Unmarshal([]byte(data), &input)
-		if err != nil {
-			emsg := fmt.Sprintf("Error parsing data: %v", err.Error())
-			retError(w, emsg, http.StatusBadRequest)
-			return
-		}
-	}
-	err = s.DefineCluster(input)
-	if err != nil {
-		emsg := fmt.Sprintf("Error: %v", err.Error())
-		retError(w, emsg, http.StatusBadRequest)
-		return
-	}
-	cors(w, r)
-	_, err = w.Write([]byte("SUCCESS"))
-	if err != nil {
-		emsg := fmt.Sprintf("Error: %v", err.Error())
-		retError(w, emsg, http.StatusBadRequest)
-		return
-	}
-}
-
-func (s *Server) clusterEdit(w http.ResponseWriter, r *http.Request) {
-	buf := new(strings.Builder)
-	n, err := io.Copy(buf, r.Body)
-	if err != nil {
-		emsg := fmt.Sprintf("Error parsing data: %v", err.Error())
-		retError(w, emsg, http.StatusBadRequest)
-		return
-	}
-	data := buf.String()
-	var input EditClusterRequest
-	if n == 0 {
-		input = EditClusterRequest{}
-	} else {
-		err := json.Unmarshal([]byte(data), &input)
-		if err != nil {
-			emsg := fmt.Sprintf("Error parsing data: %v", err.Error())
-			retError(w, emsg, http.StatusBadRequest)
-			return
-		}
-	}
-	err = s.EditCluster(input)
-	if err != nil {
-		emsg := fmt.Sprintf("Error: %v", err.Error())
-		retError(w, emsg, http.StatusBadRequest)
-		return
-	}
-	cors(w, r)
-	_, err = w.Write([]byte("SUCCESS"))
-	if err != nil {
-		emsg := fmt.Sprintf("Error: %v", err.Error())
-		retError(w, emsg, http.StatusBadRequest)
-		return
-	}
-}
-
-func (s *Server) clusterDelete(w http.ResponseWriter, r *http.Request) {
-	buf := new(strings.Builder)
-	n, err := io.Copy(buf, r.Body)
-	if err != nil {
-		emsg := fmt.Sprintf("Error parsing data: %v", err.Error())
-		retError(w, emsg, http.StatusBadRequest)
-		return
-	}
-	data := buf.String()
-	var input DeleteClusterRequest
-	if n == 0 {
-		input = DeleteClusterRequest{}
-	} else {
-		err := json.Unmarshal([]byte(data), &input)
-		if err != nil {
-			emsg := fmt.Sprintf("Error parsing data: %v", err.Error())
-			retError(w, emsg, http.StatusBadRequest)
-			return
-		}
-	}
-	err = s.DeleteCluster(input)
-	if err != nil {
-		emsg := fmt.Sprintf("Error: %v", err.Error())
-		retError(w, emsg, http.StatusBadRequest)
-		return
-	}
-	cors(w, r)
-	_, err = w.Write([]byte("SUCCESS"))
-	if err != nil {
-		emsg := fmt.Sprintf("Error: %v", err.Error())
-		retError(w, emsg, http.StatusBadRequest)
-		return
-	}
-
-}
-
-/********* END CLUSTER *********/
